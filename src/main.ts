37afdbc8e04d06a4f9873ea5f048eb8eb61b4057
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2025-08-05 21:26:29 -0300

    feat: Ajuste no main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
-    const port = configService.get<number>('PORT') || 3003;
+    const port = configService.get<number>('PORT') || 3000;
 
     app.enableCors({
       origin: [
@@ -18,10 +18,6 @@ async function bootstrap() {
         'http://localhost:3002',
       ],
     });
-
-    logger.log('Starting all microservices...');
-    await app.startAllMicroservices();
-    logger.log('All microservices started successfully.');
 
     await app.listen(port);
     logger.log(`Application is running successfully on port: ${port}`);
